4cd35c33705a82c82112bbb595abadc14d23ea26
Kejnnary <kejnnary@mail.ru>
2017-02-24 14:52:09 +0200

    add MenuGroupsModel

---
+++ pizza2017/pizza2017/MenuGroupsModel.swift
@@ -0,0 +1,23 @@
+//
+//  MenuGroupsModel.swift
+//  pizza2017
+//
+//  Created by user on 24.02.17.
+//  Copyright © 2017 GeekHub. All rights reserved.
+//
+
+import Foundation
+
+class MenuGroupsModel {
+    var key: String
+    var name: String
+    var photoName: String
+    var photoUrl: String
+    
+    init(key: String, name: String, photoName: String, photoUrl: String) {
+        self.key = key
+        self.name = name
+        self.photoName = photoName
+        self.photoUrl = photoUrl
+    }
+}
