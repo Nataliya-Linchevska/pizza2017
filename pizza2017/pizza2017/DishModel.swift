0896de189b4b6133d2886a6251e0c704da54ce77
Kejnnary <kejnnary@mail.ru>
2017-03-03 12:30:46 +0200

    start work with Dish screen. Init DishMidel

---
+++ pizza2017/pizza2017/DishModel.swift
@@ -0,0 +1,29 @@
+//
+//  DishModel.swift
+//  pizza2017
+//
+//  Created by user on 03.03.17.
+//  Copyright © 2017 GeekHub. All rights reserved.
+//
+
+import Foundation
+
+class DishesModel {
+    var name: String
+    var description: String
+    var price: Float
+    var photoUrl: String
+    var photoName: String
+    var keyGroup: String
+    var key: String
+    
+    init(name: String, description: String, price: Float, photoUrl: String, photoName: String, keyGroup: String, key: String) {
+        self.name = name
+        self.description = description
+        self.price = price
+        self.photoUrl = photoUrl
+        self.photoName = photoName
+        self.keyGroup = keyGroup
+        self.key = key
+    }
+}
