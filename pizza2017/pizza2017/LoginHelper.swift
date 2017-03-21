c93973fa423c7d27bc3681b341c26b802071ae35
konstantin <isecognito@gmail.com>
2017-03-21 18:02:59 +0200

    no message

---
+++ pizza2017/pizza2017/LoginHelper.swift
@@ -0,0 +1,22 @@
+//
+//  LoginHelper.swift
+//  pizza2017
+//
+//  Created by konstantin on 3/21/17.
+//  Copyright © 2017 GeekHub. All rights reserved.
+//
+
+import Foundation
+
+class LoginHelper {
+    
+    //MARK: Init singleton
+    
+    static let instance = LoginHelper()
+    private init() {}
+    
+    //MARK: Properties
+    
+    let isAdminLogged = true
+    
+}
