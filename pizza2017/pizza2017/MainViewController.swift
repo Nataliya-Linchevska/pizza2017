362f3a046d69df3772f6b48ed32e5f4857eea486
Nataliya Linchevskaya <nata_komp@mail.ru>
2017-04-14 12:57:13 +0300

    First button

--- pizza2017/pizza2017/MainViewController.swift
+++ pizza2017/pizza2017/MainViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var shadowButtonView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        shadowButtonView.layer.shadowOpacity = 0.8
+        shadowButtonView.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
+        shadowButtonView.layer.cornerRadius = 22
+
+
+
     }
 
     override func didReceiveMemoryWarning() {
